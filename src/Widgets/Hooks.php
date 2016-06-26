639578664eb6b75c810811568b3019ca0f35150b
rozklad <jan.rozklad@gmail.com>
2016-06-26 20:46:58 +0200

    automatic commit

--- src/Widgets/Hooks.php
+++ src/Widgets/Hooks.php
@@ -6,10 +6,12 @@ class Hooks {
 
     public function search($term = null)
     {
+        // If anything else was passed down to $term, let's filter it out
+        if ( !is_string($term) )
+            $term = null;
 
-        if ( !isset($per_page) ) {
+        if ( !isset($per_page) )
             $per_page = config('sanatorium-shop.per_page');
-        }
 
         $products = Product::ordered()
             ->search()
--- themes/frontend/default/packages/sanatorium/shop/views/widgets/search.blade.php
+++ themes/frontend/default/packages/sanatorium/shop/views/widgets/search.blade.php
@@ -2,8 +2,8 @@
 
 {{-- Products search results --}}
 
-@if ( $products )
+@if ( count($products) )
     @include('sanatorium/shop::catalog/row')
 @else
-    {{ trans('sanatorium/search::general.no_search_results', ['term' => $term]) }}
+    {!! trans('sanatorium/search::general.no_search_results', ['term' => $term]) !!}
 @endif
